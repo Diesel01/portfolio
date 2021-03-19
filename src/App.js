743c2df809be385a1f1dcd0e8a1e023fd421d359
Diesel01 <matheusdiesel01@gmail.com>
2021-03-19 08:30:57 -0300

    Created a Footer module, but still haven't added it to App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {en, de} from "./languages.js";
 import Header from './components/header.js'
 import AboutMe from "./components/aboutme.js";
 import WebDev from "./components/webDev.js";
-import Footer from "./components/footer.js";
 
 import { secondH3, h3Div, backgroundDivH3 } from "./styles/App.module.css";
 
@@ -14,24 +13,18 @@ class App extends React.Component {
     }
 
   componentDidMount(){ 
-      // let lang = navigator.language; 
-      // document.documentElement.lang = lang; 
-  
-      // let prop = lang.split("-")
-      // console.log(prop[0])
-      // this.setState( {lang: prop[0]} )
     if (navigator.language === "de-DE" || "de-AT" || "de-CH"){ 
       document.documentElement.lang = "de";
       this.setState({ lang: de })
     }
-    else if (navigator.language === "pt-BR" || "pt-PT"){ 
-      return 
-    }
+    // else if (navigator.language === "pt-BR" || "pt-PT"){ 
+    //   return 
+    // }
   }
   
   render(){
     return(
-       <div>
+       <>
 
         <Header greetings = {this.state.lang.greetings} description = {this.state.lang.description} />
         
@@ -48,10 +41,8 @@ class App extends React.Component {
 
           {this.state.lang.webDevProjects.map((project, index) => { return (<WebDev {...project} key = {project.index}/>) })}
         </section> 
-
-        <Footer />
-
-      </div>
+      
+      </>
     )
   }
 }
